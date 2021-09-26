36c6464e7e45920e3013f30689f905dd866c2db9
jin-co <lilijk@hanmail.net>
2021-09-26 16:22:24 +0900

    chore: update

---
+++ A1_TransactionRecord/A1_TransactionRecord/Models/TransactionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1_TransactionRecord.Models
+{
+    public class TransactionContext : DbContext
+    {
+        public TransactionContext(
+            DbContextOptions<TransactionContext> options) : base(options)
+        {}
+
+        public DbSet<TransactionRecord> Transactions { get; set; }
+    }
+}
--- A1_TransactionRecord/A1_TransactionRecord/Models/TransactionRecord.cs
+++ A1_TransactionRecord/A1_TransactionRecord/Models/TransactionRecord.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace A1_TransactionRecord.Models
 {
-    public class Transaction
+    public class TransactionRecord
     {
+        public int TransactionId { get; set; }
+        [Required(ErrorMessage="")]
+        public string TicketSymbol { get; set; }
+        [Required(ErrorMessage="")]
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage="")]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage="")]
+        public double SharePrice { get; set; }
+        //public int CommissionFee { get; set; }
+        //public int GrossValue { get; set; }
+        //public int NetValue { get; set; }
+
     }
 }
