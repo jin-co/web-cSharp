f6315269c5e4bb9ca27af4b0b84b1c55b1983280
jin-co <lilijk@hanmail.net>
2021-09-26 15:56:05 +0900

    chore: update

---
+++ A1_TransactionRecord/A1_TransactionRecord/Models/TransactionRecord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1_TransactionRecord.Models
+{
+    public class Transaction
+    {
+    }
+}
--- A1_TransactionRecord/A1_TransactionRecord/Startup.cs
+++ A1_TransactionRecord/A1_TransactionRecord/Startup.cs
@@ -29,16 +29,6 @@ namespace A1_TransactionRecord
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
