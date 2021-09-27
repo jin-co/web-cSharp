d39f4afc8ea7364787da8e0fcbe78cf4d47f2270
jin-co <lilijk@hanmail.net>
2021-09-27 21:23:33 +0900

    chore: update

--- A1_TransactionRecord/A1_TransactionRecord/Migrations/20210927120056_TransactionType.Designer.cs
+++ A1_TransactionRecord/A1_TransactionRecord/Migrations/20210927120056_TransactionType.Designer.cs
@@ -3,14 +3,16 @@ using A1_TransactionRecord.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace A1_TransactionRecord.Migrations
 {
     [DbContext(typeof(TransactionContext))]
-    partial class TransactionContextModelSnapshot : ModelSnapshot
+    [Migration("20210927120056_TransactionType")]
+    partial class TransactionType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ A1_TransactionRecord/A1_TransactionRecord/Migrations/20210927120056_TransactionType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace A1_TransactionRecord.Migrations
+{
+    public partial class TransactionType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
