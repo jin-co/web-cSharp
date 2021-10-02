f32c5d9f850d0f8c9bf5e3fc2d3c993b19c16645
jin-co <lilijk@hanmail.net>
2021-10-02 22:33:26 +0900

    chore: update

---
+++ L6_RazorViewOverView/L6_RazorViewOverView/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L6_RazorViewOverView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CustomerName = "Jack";
+            return View();
+        }
+    }
+}
