efd7311156a45c452e472936d4b4a896e06b0500
jin-co <lilijk@hanmail.net>
2021-10-06 00:20:44 +0900

    chore: update

---
+++ L7_BookstoreApp/BookstoreApp/BookstoreApp/Models/BookList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreApp.Models
+{
+    public class BookList
+    {
+        public List<Book> Books { get; set; }
+        public string ActiveGenre { get; set; }
+        public string ActiveAuthor { get; set; }
+        public string ActivePublisher { get; set; }
+        private List<Genre> genres { get; set; }
+        public List<Genre> Genres
+        {
+            get => genres;
+            set
+            {
+                genres = value;
+                genres.Insert(0, new Genre { GenreID = "all", Name = "All" });
+            }
+        }
+
+        public List<Author> authors { get; set; }
+
+        public List<Author> Authors
+        {
+            get => authors;
+            set
+            {
+                authors = value;
+                authors.Insert(0, new Author { AuthorID = "all", FullName = "All" });
+            }
+        }
+        public List<Publisher> publishers { get; set; }
+
+        public List<Publisher> Publishers
+        {
+            get => publishers;
+            set
+            {
+                publishers = value;
+                publishers.Insert(0, new Publisher { PublisherID = "all", Name = "All" });
+            }
+        }
+    }
+}
