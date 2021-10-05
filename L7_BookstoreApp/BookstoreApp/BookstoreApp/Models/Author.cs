13e5e63c4040308b370dd03097ef3323d5247b47
jin-co <lilijk@hanmail.net>
2021-10-05 23:16:28 +0900

    launch: initialize book app

---
+++ L7_BookstoreApp/BookstoreApp/BookstoreApp/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreApp.Models
+{
+    public class Author
+    {
+        [Key]
+        public string AuthorID { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
