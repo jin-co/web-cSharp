ce383e226fe9c894817d7282fa0da87773a7fb93
jin-co <lilijk@hanmail.net>
2021-10-10 11:50:14 +0900

    reboot: launch from after toggle

---
+++ A2_TransactionRecord/A1_TransactionRecord/Migrations/20210926153233_Initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace A1_TransactionRecord.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransactionRecordKbaek7943s",
+                columns: table => new
+                {
+                    TransactionRecordKbaek7943Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TicketSymbol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    SharePrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionRecordKbaek7943s", x => x.TransactionRecordKbaek7943Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionRecordKbaek7943s",
+                columns: new[] { "TransactionRecordKbaek7943Id", "CompanyName", "Quantity", "SharePrice", "TicketSymbol" },
+                values: new object[] { 1, "Microsoft", 100, 123.45, "MSFT" });
+
+            migrationBuilder.InsertData(
+                table: "TransactionRecordKbaek7943s",
+                columns: new[] { "TransactionRecordKbaek7943Id", "CompanyName", "Quantity", "SharePrice", "TicketSymbol" },
+                values: new object[] { 2, "Google", 100, 2701.7600000000002, "GOOG" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionRecordKbaek7943s");
+        }
+    }
+}
---
+++ A2_TransactionRecord/A1_TransactionRecord/Startup.cs
@@ -0,0 +1,70 @@
+using A1_TransactionRecord.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1_TransactionRecord
+{
+    /*
+     * Author: Kwangjin Baek
+     * Date: 2021. Sep. 29.
+     * Description: Transaction application that shows transaction history
+     * with the functions to add, edit, or delete
+     */
+    
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // lowercase routing and appending trailing slash
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+
+            services.AddDbContext<TransactionContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TransactionContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "transaction",
+                    pattern: "{controller}/{action}/orderBy/{order}");
+                
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+            });
+        }
+    }
+}
