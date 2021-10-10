ee107dc95c05aa83f0c578cc5f53deefcfb51701
jin-co <lilijk@hanmail.net>
2021-10-10 18:27:34 +0900

    feat: add company pages
    - company controller
    - views(index, edit, delete pages)

--- A2_TransactionRecord/A2_TransactionRecord/Controllers/CompanyController.cs
+++ A2_TransactionRecord/A2_TransactionRecord/Controllers/CompanyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using A2_TransactionRecord.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,88 @@ namespace A2_TransactionRecord.Controllers
 {
     public class CompanyController : Controller
     {
+        #region Constructors
+        public CompanyController(TransactionContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+
+        #region Properties
+        private TransactionContext context { get; set; }
+        #endregion
+
+        #region Methods
         public IActionResult Index()
         {
-            return View();
+            var companies = context.Companies
+                .OrderBy(c => c.Name)
+                .ToList();
+            return View(companies);
         }
+
+        // shows add page with a form to fill in
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Companies = context.Companies.OrderBy(c => c.Name).ToList();
+            return View("Edit", new Company());
+        }
+
+        // shows edit page with a form to fill in
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Companies = context.Companies.OrderBy(c => c.Name).ToList();
+            var company = context.Companies.Find(id);
+            return View(company);
+        }
+
+        // handles post request from the add or edit page
+        // based on whether there is an ID attached
+        [HttpPost]
+        public IActionResult Edit(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.CompanyId == 0)
+                {
+                    context.Companies.Add(company);
+                }
+                else
+                {
+                    context.Companies.Update(company);
+                }
+                context.SaveChanges(); // commit
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (company.CompanyId == 0) ? "Add" : "Edit";
+                //ViewBag.Companies = context.TransactionTypes.OrderBy(t => t.Name).ToList();
+                return View(company);
+            }
+        }
+
+        // shows delete page with a confirm message
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var company = context.Companies.Find(id);
+            return View(company);
+        }
+
+        // deletes the data selected and show the main page with 
+        // chosen data deleted
+        [HttpPost]
+        public IActionResult Delete(Company company)
+        {
+            context.Companies.Remove(company);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
     }
 }
