d76c5568daffe8c240e23e76bb4710bca66f51be
jin-co <lilijk@hanmail.net>
2021-10-12 01:26:21 +0900

    chore: update

--- A2_TransactionRecord/A2_TransactionRecord/Controllers/CompanyController.cs
+++ A2_TransactionRecord/A2_TransactionRecord/Controllers/CompanyController.cs
@@ -1,96 +1,152 @@
-﻿using A2_TransactionRecord.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using A2_TransactionRecord.Models;
 
 namespace A2_TransactionRecord.Controllers
 {
     public class CompanyController : Controller
     {
-        #region Constructors
+        private readonly TransactionContext _context;
+
         public CompanyController(TransactionContext context)
         {
-            this.context = context;
-        }
-        #endregion
-
-        #region Properties
-        private TransactionContext context { get; set; }
-        #endregion
-
-        #region Methods
-        public IActionResult Index()
-        {
-            var companies = context.Companies
-                .OrderBy(c => c.Name)
-                .ToList();
-            return View(companies);
+            _context = context;
         }
 
-        // shows add page with a form to fill in
-        [HttpGet]
-        public IActionResult Add()
+        // GET: Company
+        public async Task<IActionResult> Index()
         {
-            ViewBag.Action = "Add";
-            ViewBag.Companies = context.Companies.OrderBy(c => c.Name).ToList();
-            return View("Edit", new Company());
+            return View(await _context.Companies.ToListAsync());
         }
 
-        // shows edit page with a form to fill in
-        [HttpGet]
-        public IActionResult Edit(int id)
+        // GET: Company/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            ViewBag.Action = "Edit";
-            ViewBag.Companies = context.Companies.OrderBy(c => c.Name).ToList();
-            var company = context.Companies.Find(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .FirstOrDefaultAsync(m => m.CompanyId == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
             return View(company);
         }
 
-        // handles post request from the add or edit page
-        // based on whether there is an ID attached
+        // GET: Company/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Company/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public IActionResult Edit(Company company)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CompanyId,Name,Address,TickerSymbol")] Company company)
         {
             if (ModelState.IsValid)
             {
-                if (company.CompanyId == 0)
-                {
-                    context.Companies.Add(company);
-                }
-                else
-                {
-                    context.Companies.Update(company);
-                }
-                context.SaveChanges(); // commit
-                TempData["message"] = company.Name + " Save Successful";
-                return RedirectToAction("Index", "Company");
+                _context.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Company/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
             }
 
-            ViewBag.Action = (company.CompanyId == 0) ? "Add" : "Edit";
-            TempData["message"] = company.Name + " Save Failed";
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
             return View(company);
         }
 
-        // shows delete page with a confirm message
-        [HttpGet]
-        public IActionResult Delete(int id)
-        {
-            var company = context.Companies.Find(id);
-            return View(company);
-        }
-
-        // deletes the data selected and show the main page with 
-        // chosen data deleted
+        // POST: Company/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public IActionResult Delete(Company company)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CompanyId,Name,Address,TickerSymbol")] Company company)
         {
-            context.Companies.Remove(company);
-            context.SaveChanges();
-            TempData["message"] = company.Name + " Delete Successful";
-            return RedirectToAction("Index", "Company");
+            if (id != company.CompanyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(company);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompanyExists(company.CompanyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
         }
-        #endregion
+
+        // GET: Company/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .FirstOrDefaultAsync(m => m.CompanyId == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // POST: Company/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies.Any(e => e.CompanyId == id);
+        }
     }
 }
