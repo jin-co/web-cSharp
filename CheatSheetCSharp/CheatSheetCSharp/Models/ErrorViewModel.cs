75e359189b1abf51831d680027869099db2a461c
jin-co <lilijk@hanmail.net>
2021-10-17 15:21:27 +0900

    launch: initialize c sharp cheat sheet

---
+++ CheatSheetCSharp/CheatSheetCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CheatSheetCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
