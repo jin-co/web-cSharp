e7861fac46cbb513996544a228ddd5c4150945db
jin-co <lilijk@hanmail.net>
2021-10-21 06:40:51 +0900

    feat: add position class

---
+++ midterm-starting-code/BPApp/Models/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPApp.Models
+{
+    public class Position
+    {
+        public string PositionId { get; set; }
+        public string Name { get; set; }
+    }
+}
