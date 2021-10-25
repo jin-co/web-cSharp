ec666d43e7c9fc75d345ed1643af16f2ae6ae1bb
jin-co <lilijk@hanmail.net>
2021-10-25 14:01:50 +0900

    chore: update

---
+++ CheatSheetCSharp/CheatSheetCSharp/Controllers/RazorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheatSheetCSharp.Controllers
+{
+    public class RazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Tom";
+            ViewBag.Categories = new List<string>
+            {
+                "C1",
+                "C2",
+                "C3",
+            };
+            return View();
+        }
+    }
+}
