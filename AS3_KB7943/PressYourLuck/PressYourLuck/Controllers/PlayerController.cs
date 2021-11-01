f1edaadf85979677d5b5d163791f87164dcb592e
jin-co <lilijk@hanmail.net>
2021-11-01 21:43:19 +0900

    chore: create player index page

--- AS3_KB7943/PressYourLuck/PressYourLuck/Controllers/PlayerController.cs
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace PressYourLuck.Controllers
 {
     public class PlayerController : Controller
     {
-        
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1.00, 10000.00)]
+        public double StartingCoins { get; set; }
     }
 }
