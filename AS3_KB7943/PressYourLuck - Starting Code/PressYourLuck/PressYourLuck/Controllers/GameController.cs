b06c1f3e377e4ae79efe8bce28a18cff302763f7
jin-co <lilijk@hanmail.net>
2021-11-01 20:23:27 +0900

    launch: initialize assignment 3

---
+++ AS3_KB7943/PressYourLuck - Starting Code/PressYourLuck/PressYourLuck/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PressYourLuck.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressYourLuck.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            var tileList = GameHelper.GenerateNewGame();
+            return View(tileList);
+        }
+    }
+}
