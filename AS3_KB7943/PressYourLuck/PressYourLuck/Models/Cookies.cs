56c71aa23f77aef48db2d079950f89085e44b977
jin-co <lilijk@hanmail.net>
2021-11-02 00:00:41 +0900

    chore: update

--- AS3_KB7943/PressYourLuck/PressYourLuck/Models/Cookies.cs
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Models/Cookies.cs
@@ -8,6 +8,8 @@ namespace PressYourLuck.Models
 {
     public class Cookies
     {
+        private const string USER_KEY = "user";
+
         private IRequestCookieCollection requestCookies { get; set; }
         private IResponseCookies responseCookies { get; set; }
 
@@ -19,6 +21,19 @@ namespace PressYourLuck.Models
         public Cookies(IRequestCookieCollection requestCookies)
         {
             this.requestCookies = requestCookies;
+        }
+
+        public void SetMyTeamIds(string name)
+        {
+            string userName = name;
+            CookieOptions options = new CookieOptions { Expires = DateTime.Now.AddDays(30) };
+            RemovePreviousUser();     // delete old cookie first
+            responseCookies.Append(USER_KEY, userName, options);
+        }
+
+        private void RemovePreviousUser()
+        {
+            responseCookies.Delete(USER_KEY);
         }
     }
 }
