2fd9346ac480a5964a5693f51b9cb6352705d5d4
jin-co <lilijk@hanmail.net>
2021-11-03 00:06:42 +0900

    chore: update

--- AS3_KB7943/PressYourLuck/PressYourLuck/Startup.cs
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Startup.cs
@@ -29,8 +29,6 @@ namespace PressYourLuck
 
             // json
             services.AddControllersWithViews().AddNewtonsoftJson();
-
-            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
