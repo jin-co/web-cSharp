a1b0b83a930eaf50f6e64fb6dd67a93bb6fc58e8
jin-co <lilijk@hanmail.net>
2021-11-03 19:08:21 +0900

    chore: update

---
+++ WebApplication1/WebApplication1/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Test
+    {
+        [Required(ErrorMessage = "required")]        
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "required")]
+        public string Address { get; set; }
+    }
+}
