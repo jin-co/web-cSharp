b0a7edac6e48ef18e9d95a978420246be93b3a01
jin-co <lilijk@hanmail.net>
2021-11-05 01:28:58 +0900

    chore: complete context file

--- ETicket/ETicket/Data/AppDbContext.cs
+++ ETicket/ETicket/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ETicket.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,38 @@ namespace ETicket.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // setting connection between many to many
+            modelBuilder.Entity<ActorMovie>().HasKey(am => new
+            {
+                // setting compound key
+                am.ActorId,
+                am.MovieId
+            });
+
+            // connecting movie to actor movie table
+            modelBuilder.Entity<ActorMovie>()
+                .HasOne(m => m.Movie)
+                .WithMany(am => am.ActorMovies)
+                .HasForeignKey(m => m.MovieId);
+
+            // connecting Actor to actor movie table
+            modelBuilder.Entity<ActorMovie>()
+                .HasOne(a => a.Actor)
+                .WithMany(am => am.ActorMovies)
+                .HasForeignKey(a => a.ActorId);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        // lastly I have to define table names for each model
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
