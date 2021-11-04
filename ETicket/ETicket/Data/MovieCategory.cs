70f6295506ff81d979c4daea2fbcc1b32acc7a44
jin-co <lilijk@hanmail.net>
2021-11-05 00:55:07 +0900

    chore: add movie class

---
+++ ETicket/ETicket/Data/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,  // changing starting index(default: 0)
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
---
+++ ETicket/ETicket/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using ETicket.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
