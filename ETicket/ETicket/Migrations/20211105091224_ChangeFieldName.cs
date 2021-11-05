d81d412cfe6b9a2026c9099f33ae2844040ed187
jin-co <lilijk@hanmail.net>
2021-11-05 18:13:22 +0900

    chore: seed data

---
+++ ETicket/ETicket/Migrations/20211105091224_ChangeFieldName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETicket.Migrations
+{
+    public partial class ChangeFieldName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Movies",
+                newName: "ImageURL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Movies",
+                newName: "Image");
+        }
+    }
+}
