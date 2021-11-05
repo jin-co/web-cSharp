a07d9b1335d5646269c2f9737db21ccdaf368d80
jin-co <lilijk@hanmail.net>
2021-11-05 19:32:17 +0900

    chore: add cinemas controller

---
+++ ETicket/ETicket/Controllers/CinemasController.cs
@@ -0,0 +1,26 @@
+﻿using ETicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public CinemasController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
