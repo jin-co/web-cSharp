64de232cd6d1082021cd19ee3cd6801ed7c055a3
jin-co <lilijk@hanmail.net>
2021-11-05 19:33:55 +0900

    chore: add movies controller

---
+++ ETicket/ETicket/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using ETicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public MoviesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Moives = await context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
