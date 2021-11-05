ccabd79beaef6308e1afe112e1d64e4006ba5be8
jin-co <lilijk@hanmail.net>
2021-11-05 19:23:14 +0900

    chore: add actor controller

---
+++ ETicket/ETicket/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using ETicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public ActorsController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = context.Actors.ToList();
+            return View();
+        }
+    }
+}
