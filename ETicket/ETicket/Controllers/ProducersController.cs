6030547e4f19a2a5ba3fb841c62a974010d99cc3
jin-co <lilijk@hanmail.net>
2021-11-05 19:27:38 +0900

    chore: add producers controller

---
+++ ETicket/ETicket/Controllers/ProducersController.cs
@@ -0,0 +1,27 @@
+﻿using ETicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Controllers
+{    
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public ProducersController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // getting data asynchronously
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
