85e7a1067ea987f2710e6173e0cedd12d44d1e44
jin-co <lilijk@hanmail.net>
2021-11-07 10:23:15 +0900

    chore: update

--- midterm-starting-code/BPApp/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    $('input[type=datetime]').datepicker({
-        dateFormat: "m/d/yy",
-        changeMonth: true,
-        changeYear: true,
-        yearRange: "-60:+0"
-    });
-});
