35666eb4e4db90a6d0daf651dbb78157a5806b8f
jin-co <lilijk@hanmail.net>
2021-11-09 21:50:44 +0900

    chore: finish UI for part 7, 8

--- AS3_KB7943/PressYourLuck/PressYourLuck/Controllers/AuditController.cs
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Controllers/AuditController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PressYourLuck.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PressYourLuck.Controllers
-{
+{    
     public class AuditController : Controller
     {
+        private readonly PressLuckContext context;
 
+        public AuditController(PressLuckContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var audit = context.Audits
+                .Include(a => a.AuditTypes)
+                .ToList();
+            return View(audit);
+        }
     }
 }
--- AS3_KB7943/PressYourLuck/PressYourLuck/Models/Audit.cs
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Models/Audit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,13 +10,20 @@ namespace PressYourLuck.Models
     public class Audit
     {
         public int AuditId { get; set; }
+
+        [DisplayName("Player Name")]
         public string PlayerName { get; set; }
+
+        [DisplayName("Created")]
         public DateTime CreatedDate { get; set; }
+
         public double Amount { get; set; }
 
         // Relation
         [ForeignKey("AuditTypeId")]
         public string AuditTypeId { get; set; }
+
+        [DisplayName("Audit Type")]
         public AuditType AuditTypes { get; set; }
     }
 }
