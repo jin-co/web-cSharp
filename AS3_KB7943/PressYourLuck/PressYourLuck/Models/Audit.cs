df311a109f0f932c090283a525c93fef9d701b60
jin-co <lilijk@hanmail.net>
2021-11-09 20:28:04 +0900

    chore: install packages for DB

---
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Models/Audit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressYourLuck.Models
+{
+    public class Audit
+    {
+        public int AutidId { get; set; }
+        public string PlayerName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public double Amount { get; set; }
+
+        // Relation
+        [ForeignKey("AuditTypeId")]
+        public string AuditTypeId { get; set; }
+        public AuditType AuditTypes { get; set; }
+    }
+}
