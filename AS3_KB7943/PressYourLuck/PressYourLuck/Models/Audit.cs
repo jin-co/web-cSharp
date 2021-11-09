91f4d620659bbabf991bf193f1400e6a86d0fc64
jin-co <lilijk@hanmail.net>
2021-11-09 21:15:45 +0900

    chore: finish migrations

--- AS3_KB7943/PressYourLuck/PressYourLuck/Models/Audit.cs
+++ AS3_KB7943/PressYourLuck/PressYourLuck/Models/Audit.cs
@@ -8,14 +8,14 @@ namespace PressYourLuck.Models
 {
     public class Audit
     {
-        public int AutidId { get; set; }
+        public int AuditId { get; set; }
         public string PlayerName { get; set; }
         public DateTime CreatedDate { get; set; }
         public double Amount { get; set; }
 
         // Relation
-        //[ForeignKey("AuditTypeId")]
-        //public string AuditTypeId { get; set; }
-        //public AuditType AuditTypes { get; set; }
+        [ForeignKey("AuditTypeId")]
+        public string AuditTypeId { get; set; }
+        public AuditType AuditTypes { get; set; }
     }
 }
