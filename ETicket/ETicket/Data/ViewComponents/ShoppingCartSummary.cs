5e1d8f303f52f55c1829f048ecbd045132d18f0a
jin-co <lilijk@hanmail.net>
2021-11-11 19:40:23 +0900

    chore: add component

---
+++ ETicket/ETicket/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,29 @@
+﻿using ETicket.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Data.ViewComponents
+{
+    // there are three ways to get C# recognize Viewcomponent
+    // 1: name it ~~ViewComponet
+    // 2: use decoration [ViewComponent]
+    // 3: inherit
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = shoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+    }
+}
