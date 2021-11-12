87492db8e74bca7c08f1875745ddd8daad466cbf
jin-co <lilijk@hanmail.net>
2021-11-12 19:39:45 +0900

    chore: update

---
+++ ETicket/ETicket/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using ETicket.Data;
+using ETicket.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicket.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly AppDbContext context;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, 
+            AppDbContext context)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
