cb151240c7793883ddab4258b0f8e4324756ace2
jin-co <lilijk@hanmail.net>
2021-11-21 14:36:29 +0900

    chore: add company class with view

---
+++ PayrollSystem/PayrollSystemWeb/Controllers/CompanyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayrollSystem.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            // validate inbound
+            // interect with business layer
+            // choose the view
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveDetail(string id, string name, string streetAdress)
+        {
+            // validate inbound
+            // interect with business layer
+            // choose the view
+            return View("Index");
+        }
+    }
+
+}
