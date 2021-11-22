d0123011bcfef10c0a95a21d713fb7d761039602
jin-co <lilijk@hanmail.net>
2021-11-22 22:25:02 +0900

    chore: update

--- A4_AccountsPayable/A4_AccountsPayable/Controllers/HomeController.cs
+++ A4_AccountsPayable/A4_AccountsPayable/Controllers/HomeController.cs
@@ -38,12 +38,40 @@ namespace A4_AccountsPayable.Controllers
         }
 
 
-        public IActionResult VendorList()
+        public IActionResult VendorList(string vendorFilter)
         {
+            // vendors
             var vendors = new List<Vendor>();
             vendors = context.Vendors.ToList();
+
+            // vendor names
+            List<string> vendorNameList = 
+                vendors.Select(s => s.VendorName).Distinct().ToList();
+
+            switch (vendorFilter)
+            {
+                case "ae":
+                    vendorNameList.Where(a => a.StartsWith("A"));
+                    break;
+                case "fk":
+                    vendorNameList.Where(a => a.ToLower()[0] < 'l');
+                    break;
+                case "lr":
+                    vendorNameList.Where(a => a.ToLower()[0] < 's');
+                    break;
+                case "sz":
+                    vendorNameList.Where(a => a.ToLower()[0] >= 's');
+                    break;
+            }
+
             
-            return View(vendors);
+
+            VendorListViewModel vlvm = new VendorListViewModel()
+            {
+                Vendors = vendors,
+                VendorNameFilter = vendorNameList
+            };
+            return View("Index", vlvm);
         }
 
 
