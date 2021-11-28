9ae730444fb3bc24e127f9832cc7e2a422bfbfa7
jin-co <lilijk@hanmail.net>
2021-11-28 12:28:44 +0900

    chore: update

---
+++ EticketPractice/EticketPractice/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EticketPractice.Models
+{
+    public class Actor
+    {
+        public int ActorId { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
