b63870338b438bc9df6dcd6611362fb39c095918
jin-co <lilijk@hanmail.net>
2021-12-05 23:54:58 +0900

    chore: add component

---
+++ A5_QuarterlySalesApp/QuarterlySales/Components/TopQuartersByEmployee.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuarterlySales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterlySales.Components
+{
+    public class TopQuartersByEmployee : ViewComponent
+    {
+        private SalesContext _context;
+
+        public TopQuartersByEmployee(SalesContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
