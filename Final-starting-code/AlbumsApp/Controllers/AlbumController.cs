b0170942ba35dbcb3c4e4e3cfb8914100cec8fa6
jin-co <lilijk@hanmail.net>
2021-12-14 23:08:21 +0900

    launch: init final

---
+++ Final-starting-code/AlbumsApp/Controllers/AlbumController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AlbumsApp.Models;
+using AlbumsApp.Models.ViewModels;
+
+namespace AlbumsApp.Controllers
+{
+    public class AlbumController : Controller
+    {
+        public AlbumController(AlbumsDbContext albumsDbContext)
+        {
+            _albumsDbContext = albumsDbContext;
+        }
+
+        [Route("/")]
+        public IActionResult List()
+        {
+            var albums = _albumsDbContext.Albums.Include(a => a.Studio).OrderByDescending(a => a.YearProduced).ToList();
+            return View(albums);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var viewModel = new AddOrEditAlbumViewModel();
+            viewModel.AllStudios = GetAllStudios();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddOrEditAlbumViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _albumsDbContext.Albums.Add(viewModel);
+                _albumsDbContext.SaveChanges();
+
+                return RedirectToAction("List", "Album");
+            }
+            else
+            {
+                ModelState.AddModelError("", "There were errors in the form - please fix them and try adding again.");
+                viewModel.AllStudios = GetAllStudios();
+                return View(viewModel);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            AddOrEditAlbumViewModel viewModel = new AddOrEditAlbumViewModel(_albumsDbContext.Albums.Include(a => a.Studio).Where(a => a.AlbumId == id).FirstOrDefault());
+            viewModel.AllStudios = GetAllStudios();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AddOrEditAlbumViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _albumsDbContext.Albums.Update(viewModel);
+                _albumsDbContext.SaveChanges();
+
+                return RedirectToAction("List", "Album");
+            }
+            else
+            {
+                ModelState.AddModelError("", "There were errors in the form - please fix them and try updating again.");
+                viewModel.AllStudios = GetAllStudios();
+                return View(viewModel);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DeleteConfirmation(int id)
+        {
+            Album album = _albumsDbContext.Albums.Include(a => a.Studio).Where(a => a.AlbumId == id).FirstOrDefault();
+            return View(album);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Album album = _albumsDbContext.Albums.Include(a => a.Studio).Where(a => a.AlbumId == id).FirstOrDefault();
+            _albumsDbContext.Albums.Remove(album);
+            _albumsDbContext.SaveChanges();
+
+            return RedirectToAction("List", "Album");
+        }
+
+        [HttpGet("/Error")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private List<Studio> GetAllStudios()
+        {
+            return _albumsDbContext.Studios.OrderBy(s => s.Name).ToList();
+        }
+
+        private AlbumsDbContext _albumsDbContext;
+    }
+}
---
+++ Final-starting-code/AlbumsApp/Controllers/StudioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AlbumsApp.Models;
+
+namespace AlbumsApp.Controllers
+{
+    public class StudioController : Controller
+    {
+        public StudioController(AlbumsDbContext albumsDbContext)
+        {
+            _albumsDbContext = albumsDbContext;
+        }
+
+        public IActionResult List()
+        {
+            List<Studio> studios = _albumsDbContext.Studios.OrderBy(s => s.Name).ToList();
+            return View(studios);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new Studio());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Studio studio)
+        {
+            _albumsDbContext.Studios.Add(studio);
+            _albumsDbContext.SaveChanges();
+
+            return RedirectToAction("List", "Album");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Studio studio = _albumsDbContext.Studios.Where(a => a.StudioId == id).FirstOrDefault();
+            return View(studio);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Studio studio)
+        {
+            _albumsDbContext.Studios.Update(studio);
+            _albumsDbContext.SaveChanges();
+
+            return RedirectToAction("List", "Album");
+        }
+
+        private AlbumsDbContext _albumsDbContext;
+    }
+}
