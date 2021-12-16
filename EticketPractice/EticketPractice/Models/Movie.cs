ccf3f4aebef958282d4b93272d08b8091790717d
jin-co <lilijk@hanmail.net>
2021-12-16 13:39:57 +0900

    chore: update

---
+++ EticketPractice/EticketPractice/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EticketPractice.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
---
+++ EticketPractice/EticketPractice/Models/Producer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EticketPractice.Models
+{
+    public class Producer
+    {        
+        public int ProducerId { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
