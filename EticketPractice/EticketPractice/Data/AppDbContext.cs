dcf7222781bbe4b2e1d8f97b7ee17a8483fa68bf
jin-co <lilijk@hanmail.net>
2021-12-17 12:13:46 +0900

    chore: update

--- EticketPractice/EticketPractice/Data/AppDbContext.cs
+++ EticketPractice/EticketPractice/Data/AppDbContext.cs
@@ -13,6 +13,12 @@ namespace EticketPractice.Data
         {
         }
 
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // creating compound primary key
@@ -22,13 +28,15 @@ namespace EticketPractice.Data
                 am.MovieId
             });
 
-            // setting foreign key
-            modelBuilder.Entity<ActorMovie>().HasOne(m => m.Movie).WithMany(am => am.ActorMovies)
+            // setting relationship many to many
+            modelBuilder.Entity<ActorMovie>().HasOne(m => m.Movie)
+                .WithMany(am => am.ActorMovies)
                 .HasForeignKey(m => m.MovieId);
-            modelBuilder.Entity<ActorMovie>().HasOne(m => m.Actor).WithMany(am => am.ActorMovies)
+            modelBuilder.Entity<ActorMovie>().HasOne(m => m.Actor)
+                .WithMany(am => am.ActorMovies)
                 .HasForeignKey(m => m.ActorId);
 
             base.OnModelCreating(modelBuilder);
-        }
+        }        
     }
 }
--- EticketPractice/EticketPractice/Startup.cs
+++ EticketPractice/EticketPractice/Startup.cs
@@ -2,6 +2,7 @@ using EticketPractice.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +25,9 @@ namespace EticketPractice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>();
+            // Dbcontext
+            services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("MovieDBContext")));
 
             services.AddControllersWithViews();
         }
