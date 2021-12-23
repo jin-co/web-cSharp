22117bc7392d84191c3b35fd081ad601d5a073f1
jin-co <lilijk@hanmail.net>
2021-12-23 20:07:40 +0900

    chore: update

---
+++ CheatSheetCSharp/CheatSheetCSharp/Areas/BMI/Controllers/BmiController.cs
@@ -0,0 +1,28 @@
+﻿using CheatSheetCSharp.Areas.BMI.Models;
+using CheatSheetCSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheatSheetCSharp.Areas.BMI.Controllers
+{
+    [Area("BMI")]
+    public class BmiController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.bmi = 0.00;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(BmiModel bmiModel)
+        {
+            ViewBag.bmi = bmiModel.CalculateBMI();
+            return View(bmiModel);
+        }
+    }
+}
