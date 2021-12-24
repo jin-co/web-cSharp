c06b13c6c9fb9705fb00b9c23966ccbdc9830b8f
jin-co <lilijk@hanmail.net>
2021-12-24 14:07:57 +0900

    chore: update

---
+++ CheatSheetCSharp/CheatSheetCSharp/Areas/Validation/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CheatSheetCSharp.Areas.Validation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheatSheetCSharp.Areas.Validation.Controllers
+{
+    [Area("Validation")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Result = "Success";
+            }
+            else
+            {
+                ViewBag.Result = "Failed";
+            }
+
+            return View(userModel);
+        }
+    }
+}
