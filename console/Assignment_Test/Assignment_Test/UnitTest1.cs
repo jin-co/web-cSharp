73e5b6135085cc4d47b6f1a65e810f3292dbac69
jin-co <lilijk@hanmail.net>
2022-03-29 23:26:06 -0400

    Added tests cases

--- console/Assignment_Test/Assignment_Test/UnitTest1.cs
+++ console/Assignment_Test/Assignment_Test/UnitTest1.cs
@@ -26,18 +26,205 @@ namespace Assignment_Test
         }
 
         [Test]
-        public void PageTitle_Assignment3_ReturnTre()
+        public void MainPage_CheckNewBoxDisplayed_NewBoxExist()
         {
             //driver.Url = "";
             driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\index.html";
 
-            driver.FindElement(By.ClassName("main-card")).Click();
+            var newComponent = driver.FindElement(By.ClassName("new"));            
+
+            Assert.That(newComponent.Displayed, Is.True);
+        }
+
+        [Test]
+        public void MainPage_CheckSearchBoxDisplayed_SearchBoxExist()
+        {
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\index.html";
+
+            var newComponent = driver.FindElement(By.ClassName("search"));
+
+            Assert.That(newComponent.Displayed, Is.True);
+        }
+
+        [Test]
+        public void AddNewPage_CheckFormDisplayed_FormExist()
+        {
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\sale.html";
+
+            var newComponent = driver.FindElement(By.ClassName("form"));
+
+            Assert.That(newComponent.Displayed, Is.True);
+        }
+
+        [Test]
+        public void SummaryPage_CheckSummaryDisplayed_SummaryExist()
+        {
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\summary.html";
+
+            var newComponent = driver.FindElement(By.ClassName("table-box"));
+
+            Assert.That(newComponent.Displayed, Is.True);
+        }
+
+        [Test]
+        public void ListPage_CheckListDisplayed_ListExist()
+        {
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\list.html";
+
+            var newComponent = driver.FindElement(By.ClassName("summary-container"));
+
+            Assert.That(newComponent.Displayed, Is.True);
+        }
+
+        [Test]
+        public void SalePage_CheckErrorMessageWhenStart_ErrorMessageNotDisplayed()
+        {
+            
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\sale.html";            
+            
+            var error = driver.FindElement(By.ClassName("error-message"));            
+            driver.FindElement(By.ClassName("btn-warning"));
+
+            Assert.That(error.Displayed, Is.False);
+        }
+
+        [Test]
+        public void SalePage_ShowErrorMessageWhenEnteredNothing_ShowErrorMessage()
+        {
+
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\sale.html";
+
+            var error = driver.FindElement(By.ClassName("error-message"));
+            driver.FindElement(By.ClassName("btn-warning")).Click();
+            var summaryLink = driver.FindElement(By.LinkText("http://127.0.0.1:5500/pages/summary.html"));
+            TestContext.Progress.WriteLine("summary link ****** " + summaryLink);
+            Assert.That(error.Displayed, Is.False);
+        }
+
+        [Test]
+        public void SalePage_WhenValuesAreValid_ErrorMessageNotDisplayed()
+        {
+
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\sale.html";
+
+            var first = driver.FindElement(By.Id("first-name"));
+            var last = driver.FindElement(By.Id("last-name"));
+            var address = driver.FindElement(By.Id("address"));
+            var city = driver.FindElement(By.Id("city"));
+            var province = driver.FindElement(By.Id("province"));
+            var postal = driver.FindElement(By.Id("postal"));
+            var phone = driver.FindElement(By.Id("phone"));
+            var email = driver.FindElement(By.Id("email"));
+            var make = driver.FindElement(By.Id("make"));
+            var model = driver.FindElement(By.Id("model"));
+            var year = driver.FindElement(By.Id("year"));
+
+            first.SendKeys("Kwangjin");
+            last.SendKeys("Baek");
+            address.SendKeys("Somewhere");
+            city.SendKeys("Toronto");
+            province.SendKeys("Ontario");
+            postal.SendKeys("N2N 3R9");
+            phone.SendKeys("000-000-0000");
+            email.SendKeys("lili@com.com");
+            make.SendKeys("Toyota");
+            model.SendKeys("Well");
+            year.SendKeys("2000");
+
+            var error = driver.FindElement(By.ClassName("error-message"));
+            driver.FindElement(By.ClassName("btn-warning")).Click();
+
+            Assert.That(error.Displayed, Is.False);
+        }
+
+        [Test]
+        public void SalePage_WhenPostalCodeIsNotValid_ShowErrorMessage()
+        {
+
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\sale.html";
+
+            var first = driver.FindElement(By.Id("first-name"));
+            var last = driver.FindElement(By.Id("last-name"));
+            var address = driver.FindElement(By.Id("address"));
+            var city = driver.FindElement(By.Id("city"));
+            var province = driver.FindElement(By.Id("province"));
+            var postal = driver.FindElement(By.Id("postal"));
+            var phone = driver.FindElement(By.Id("phone"));
+            var email = driver.FindElement(By.Id("email"));
+            var make = driver.FindElement(By.Id("make"));
+            var model = driver.FindElement(By.Id("model"));
+            var year = driver.FindElement(By.Id("year"));
+
+            first.SendKeys("Kwangjin");
+            last.SendKeys("Baek");
+            address.SendKeys("Somewhere");
+            city.SendKeys("Toronto");
+            province.SendKeys("Ontario");
+            postal.SendKeys("N2N 3R9");
+            phone.SendKeys("000-000-0000");
+            email.SendKeys("lili@com.com");
+            make.SendKeys("Toyota");
+            model.SendKeys("Well");
+            year.SendKeys("2000");
+
+            var error = driver.FindElement(By.ClassName("error-message"));
+            driver.FindElement(By.ClassName("btn-warning")).Submit();
+
+            Assert.That(error.Displayed, Is.False);
+        }
+
+        [Test]
+        public void SalePage_WhenPhoneNumIsNotValid_ShowErrorMessage()
+        {
+
+            //driver.Url = "";
+            driver.Url = "C:\\Users\\jin\\Documents\\GitHub\\practice_javascript\\QA_assignment3\\pages\\sale.html";
+
+            var first = driver.FindElement(By.Id("first-name"));
+            var last = driver.FindElement(By.Id("last-name"));
+            var address = driver.FindElement(By.Id("address"));
+            var city = driver.FindElement(By.Id("city"));
+            var province = driver.FindElement(By.Id("province"));
+            var postal = driver.FindElement(By.Id("postal"));
+            var phone = driver.FindElement(By.Id("phone"));
+            var email = driver.FindElement(By.Id("email"));
+            var make = driver.FindElement(By.Id("make"));
+            var model = driver.FindElement(By.Id("model"));
+            var year = driver.FindElement(By.Id("year"));
+
+            first.SendKeys("Kwangjin");
+            last.SendKeys("Baek");
+            address.SendKeys("Somewhere");
+            city.SendKeys("Toronto");
+            province.SendKeys("Ontario");
+            postal.SendKeys("N2N 3R9");
+            phone.SendKeys("000-000-0000");
+            email.SendKeys("lili@com.com");
+            make.SendKeys("Toyota");
+            model.SendKeys("Well");
+            year.SendKeys("2000");
+
+            var error = driver.FindElement(By.ClassName("error-message"));
+            driver.FindElement(By.ClassName("btn-warning")).Submit();
+
+            //test
+            TestContext.Progress.WriteLine(driver.Url);
+
+            Assert.That(error.Displayed, Is.False);
         }
 
         [TearDown]
         public void Close()
         {
-            //driver.Close();
+            driver.Close();
         }
     }
 }
