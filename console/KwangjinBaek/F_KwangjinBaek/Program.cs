e9fc938712f2b7af9990e031602bfe054704cf5b
jin-co <lilijk@hanmail.net>
2022-04-23 13:46:48 -0400

    Update

---
+++ console/KwangjinBaek/F_KwangjinBaek/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace KwangjinBaek
+{
+    class Program
+    {
+        //Kwangjin Baek #8707943
+        static void Main(string[] args)
+        {
+            string inputString, anotherInput, output;
+            bool goOn = true;
+
+            do
+            {
+                try
+                {
+                    // user input
+                    Console.Write("Please enter the string: ");
+                    inputString = Console.ReadLine();
+
+                    // checks if it is empty. If it is not shows the result and ends
+                    if (inputString != "")
+                    {
+                        goOn = false;
+                        output = Utility.StringOperations(inputString);
+                        Console.WriteLine(output);
+
+                        // splits the result by delimiter ','
+                        string[] tokens = output.Split(',');
+                        foreach (var i in tokens)
+                        {
+                            Console.Write(i);
+                        }
+
+                        // shows the result after spliting
+                        for (int i = 0; i < inputString.Length; i++)
+                        {
+                            if (char.IsDigit(inputString[i]))
+                            {
+                                Console.WriteLine(inputString[i]);
+                            }
+                        }
+                    }
+
+                    // asks if they want to keep going
+                    Console.Write("Would you like to stop? (enter 'yes' to stop): ");
+                    anotherInput = Console.ReadLine();
+                    if (anotherInput == "yes" || anotherInput == "YES" || anotherInput == "y" || anotherInput == "Y")
+                    {
+                        goOn = false;
+                    }
+
+                    // throws an error if it is empty
+                    else
+                    {
+                        throw new ArgumentException("Error Message");
+                    }
+                }
+
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (goOn);
+        }
+    }
+}
---
+++ console/KwangjinBaek/F_KwangjinBaek/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KwangjinBaek
+{
+    class Utility
+    {
+        // constructor
+        Utility()
+        {
+
+        }
+
+        public static string StringOperations(string input)
+        {
+            int found = input.IndexOf("#");
+            int found2 = input.IndexOf("!");
+            string result = input.Remove(found, found+1);
+            string result2 = input.Remove(found2, found2+1);
+            return result;
+            return result2;
+        }
+    }
+}
