e41174800e54cbc1b4f4f9e20227ea67d77e19b5
jin-co <lilijk@hanmail.net>
2022-04-23 13:42:42 -0400

    Update

---
+++ console/assignment_4/A4JSDSJB_josslyne/A4JSDSJB/A4JSDSJB/A4JSDSJB/Program.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4JSDSJB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Group Members:
+             * Josslyne Shantz - Jshantz8979@conestogac.on.ca
+             * Dalvir Singh Dalvir Singh - Dalvirsingh7574@conestogac.on.ca
+             * Jessica Broatch - Jbroatch9019@conestogac.on.ca
+             */
+
+            int brandNumber;
+            string brand, option;
+            bool end;
+            string[] brandJSDSJB = new string[3] { "", "", "" };
+            string[] tShirts = new string[12] { "", "", "", "", "", "", "", "", "", "", "", "" };
+            for (int i = 0; i < 3; i++)
+            {
+                brandNumber = i + 1;
+                Console.WriteLine("Please enter brand " + brandNumber);
+                brand = Console.ReadLine();
+                brandJSDSJB[i] = brand;
+            }
+            do
+            {
+                Console.WriteLine("A: Add new T-Shirts");
+                Console.WriteLine("B: Edit existing T-Shirts");
+                Console.WriteLine("C: Display existing T-Shirts");
+                Console.WriteLine("D: Delete exististing T-Shirt");
+                Console.WriteLine("E: End program");
+                Console.WriteLine("Enter the letter of the option you wish to do: ");
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "A":
+                    case "a":
+                        tShirts = AddTShirt(tShirts, brandJSDSJB);
+                        end = false;
+                        break;
+                    case "B":
+                    case "b":
+                        tShirts = EditTShirt(tShirts, brandJSDSJB);
+                        end = false;
+                        break;
+                    case "C":
+                    case "c":
+                        DisplayTShirt(tShirts);
+                        end = false;
+                        break;
+                    case "D":
+                    case "d":
+                        tShirts = DeleteTshirt(tShirts);
+                        end = false;
+                        break;
+                    default:
+                        end = true;
+                        break;
+                }
+
+
+            } while (end == false);
+        }
+
+        static string[] AddTShirt(string[] tShirtDetails, string[] brandJSDSJB)
+        {
+            try
+            {
+                string brand, size, tShirt, newTShirtString;
+                bool end = false;
+                int numberOfShirts = 0;
+                foreach(string i in tShirtDetails)
+                {
+                    if(i != "NONE")
+                    {
+                        numberOfShirts++;
+                    }
+                }
+                if (numberOfShirts != 12)
+                {
+                    do
+                    {
+                        bool sizeIsGood = false;
+                        int index, tShirtIndex, informationIndex = 0;
+                        do
+                        {
+                            if (informationIndex == -1)
+                            {
+                                Console.WriteLine("Please use one the brands you first entered.");
+                            }
+                            index = 0;
+                            informationIndex = -1;
+                            Console.Write("What brand is the T-Shirt you want to add? Enter 'DONE' if you wish to exit now. ");
+                            brand = Console.ReadLine();
+                            if(brand != "DONE")
+                            { 
+                                foreach (string i in brandJSDSJB)
+                                {
+                                    if (i == brand)
+                                    {
+                                        informationIndex = index;
+                                    }
+                                    index++;
+                                }
+                            }
+                            else { end = true; }
+                        } while (informationIndex == -1 && end == false);
+                        if(end == false)
+                        {
+                            do
+                            {
+                                Console.WriteLine("The sizes you can choose from are: ");
+                                Console.WriteLine("Small(1)\nMedium(2)\nLarge(3)\nExtra-Large(4)");
+                                Console.Write("What size is the T-Shirt you want to add? ");
+                                size = Console.ReadLine();
+                                sizeIsGood = SizeAuthentication(size);
+                            } while (sizeIsGood == false);
+                            index = 0;
+                            informationIndex = -1;
+                            tShirtIndex = -1;
+                            tShirt = brand + "-" + size;
+                            foreach (string i in tShirtDetails)
+                            {
+                                if (i == "NONE")
+                                {
+                                    tShirtIndex = index;
+                                }
+                                else if(i == tShirt)
+                                {
+                                    informationIndex = index;
+                                }
+                                index++;
+                            }
+                            if (informationIndex != -1)
+                            {
+                                Console.WriteLine("Error T-Shirt record already exists.");
+                            }
+                            else
+                            {
+                                tShirtDetails[tShirtIndex] = tShirt;
+                                numberOfShirts++;
+                                Console.WriteLine("T-Shirt record has been saved.");
+                            }
+                            if (numberOfShirts == 12)
+                            {
+                                Console.WriteLine("You have 12 T-Shirts now.\nGoing back to the main menu.");
+                                end = true;
+                            }
+                        }
+                    } while (end == false);
+                }
+                else
+                {
+                    Console.WriteLine("You already have 12 T-Shirts recorded.");
+                    Console.WriteLine("Going back to the main menu.");
+                }
+                return tShirtDetails;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Sorry, something went wrong.");
+                return tShirtDetails;
+            }
+        }
+
+        static string[] EditTShirt(string[] tShirtDetails, string[] brandJSDSJB)
+        {
+            try 
+            { 
+                string tShirt, brand, size;
+                int informationIndex = 1, tShirtIndex=-1, index = 0;
+                bool sizeIsGood = false, end = false;
+                Console.Write("What brand entry would you like to edit? Please enter in the format of 'Brand-Size': ");
+                tShirt = Console.ReadLine();
+                foreach(string i in tShirtDetails)
+                {
+                    if(i == tShirt)
+                    {
+                        tShirtIndex = index;
+                    }
+                    index ++;
+                }
+
+                if (tShirtIndex == -1)
+                {
+                    Console.WriteLine("Brand record not found for that entry.");
+                    return tShirtDetails;
+                }
+                else
+                {
+                    Console.WriteLine("Brand information found.");
+                    Console.WriteLine("Please enter updated information");
+                    do {
+                        if (informationIndex == 0)
+                        {
+                            Console.WriteLine("Please use one the brands you first entered.");
+                        }
+                        index = 0;
+                        Console.Write("What is the brand? Enter 'DONE' if you whish to return to the menu now. ");
+                        brand = Console.ReadLine();
+                        if (brand != "DONE")
+                        {
+                            foreach (string i in brandJSDSJB)
+                            {
+                                if (i == brand)
+                                {
+                                    informationIndex = index;
+                                }
+                                index++;
+                            }
+                        }
+                        else { end = true; }
+                    } while (informationIndex == 0 && end == false);
+                    if(end == false)
+                    {
+                        do
+                        {
+                            Console.WriteLine("The sizes you can choose from are: ");
+                            Console.WriteLine("Small(1)\nMedium(2)\nLarge(3)\nExtra-Large(4)");
+                            Console.Write("What size is the T-Shirt you want to add? ");
+                            size = Console.ReadLine();
+                            sizeIsGood = SizeAuthentication(size);
+                        } while (sizeIsGood == false);
+                        index = 0;
+                        informationIndex = -1;
+                        tShirt = brand + "-" + size;
+                        foreach (string i in tShirtDetails)
+                        {
+                            if (i == tShirt)
+                            {
+                                informationIndex = index;
+                            }
+                            index++;
+                        }
+                        if (informationIndex != -1)
+                        {
+                            Console.WriteLine("Error T-Shirt record already exists.");
+                        }
+                        else
+                        {
+                            tShirtDetails[informationIndex] = tShirt;
+                            Console.WriteLine("Record is updated.");
+                        }
+                    }
+                    return tShirtDetails;
+                }           
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry. Something went wrong.");
+                return tShirtDetails;
+            }
+        }
+
+        static string[] DeleteTshirt(string[] tShirtDetails)
+        {
+            Console.WriteLine("What Brand Entry Would You Like to Delete? ");
+            Console.Write("Please Enter In Brand-Size Format, i.e. Brand-Size ");
+            //array selection variable
+            string arraySelect = Console.ReadLine();
+            int tShirtIndex = -1, index = 0;
+            foreach (string i in tShirtDetails)
+            {
+                if (i == arraySelect)
+                {
+                    tShirtIndex = index;
+                }
+                index++;
+            }
+            if (tShirtIndex == -1)
+            {
+                Console.WriteLine("\nBrand Record Not Found For That Entry.");
+                Console.WriteLine("You Will Be Returned To Main Menu.");
+                Console.ReadKey();
+                return tShirtDetails;
+            }
+            else
+            {
+                Console.Write("\nBrand Information Found. Are You Sure You Want to Delete Y/N? ");
+                char deleteEntry = Convert.ToChar(Console.ReadLine());
+                if (deleteEntry is 'Y')
+                {
+                    tShirtDetails[tShirtIndex] = "NONE";
+                    Console.WriteLine("The Brand Information Has Been Deleted.");
+                    Console.WriteLine("You Will Be Returned To Main Menu.");
+                    Console.ReadKey();
+                }
+                else if (deleteEntry is 'N')
+                {
+                    Console.WriteLine("You Will Be Returned to Main Menu.");
+                    Console.ReadKey();
+                }
+                return tShirtDetails;
+            }
+        }
+
+        static void DisplayTShirt (string[] brandJSDSJB)
+        {
+            try
+            {
+                foreach (string i in brandJSDSJB)
+                {
+                    if (i != "" && i != "NONE")
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.WriteLine("Press enter to return to the main menu.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, something went wrong.");
+            }
+        }
+
+        static bool SizeAuthentication(string sizeString)
+        {
+        try
+        {
+            int size;
+            size = int.Parse(sizeString);
+            if (1 > size || size > 4)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please enter a size between 1 and 4.");
+            return false;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a number for the size.");
+            return false;
+        }
+        return true;
+        }
+    }
+}
---
+++ console/assignment_4/A4KBKMJW/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A4Group_17P1
+{
+    class Program
+    {
+        // Kwangjin Baek <Kbaek7943@conestogac.on.ca> Keith Muema <Kmuema1971@conestogac.on.ca> Jacob Wall <Jwall8052@conestogac.on.ca>
+
+        /* This application allows users to input brand (1 ~ 3) and size (1 ~ 4), and edit, delete */
+        static void Main(string[] args)
+        {
+            // arrays: brand, size, menu
+            string[] brandjin = new string[3], brandInput = new string[12];
+            string[] size = new string[4] { "Small(1)", "Medium(2)", "Large(3)", "XLarge(4)" };
+            string[] menu = {"A. Add new T-shirt details (Brand name & Size).",
+                "B. Edit existing T-shirt details (Brand name & Size)",
+                "C. Display all T-shirt information.", "D. Delete T-shirt information",
+                "E. Exit the program"};
+            // sentinels: main menu, sub menu
+            string brandInputTemporary = "", brandSizeCombined = "", end = "";
+            bool goOn = true, stayA = true, stayB = true, stayC = true, stayD = true;
+            int sizeInput = 0, whileCheck = 0;
+           
+            for (int i = 0; i < brandjin.Length; i++)
+            {
+                Console.Write("Enter brand: ");
+                brandjin[i] = Console.ReadLine();
+            }
+
+            // Main menu
+            while (goOn)
+            {
+                try
+                {
+                    stayA = true;
+                    stayB = true;
+                    stayC = true;
+                    stayD = true;
+
+                    Console.WriteLine("\n************************ Menu ************************");
+                    for (int i = 0; i < menu.Length; i++)
+                    {
+                        Console.WriteLine(menu[i]);
+                    }
+                    Console.Write("\nPlease choose an option: ");
+                    string choice = Console.ReadLine();
+
+                    // Option 'A': brand and size input
+                    if (choice == "A" || choice == "a")
+                    {
+                        /* Option A part: Users can enter item information ('brand - size') */
+                        while (stayA)
+                        {
+                            // asks brand name
+                            Console.Write("Enter brand name ({0} {1} {2}): ", brandjin[0], brandjin[1], brandjin[2]);
+                            brandInputTemporary = Console.ReadLine();
+                            if (brandInputTemporary != brandjin[0] && brandInputTemporary != brandjin[1] && brandInputTemporary != brandjin[2])
+                            {
+                                throw new ArgumentException("We only have three brands. Enter again.");
+                            }
+
+                            // asks size (1 ~ 4)
+                            Console.Write("Enter size (1 ~ 4): ");
+                            sizeInput = int.Parse(Console.ReadLine());
+                            if (sizeInput < 1 || sizeInput > 4)
+                            {
+                                throw new ArgumentException("Size out of range! Enter again.");
+                            }
+                            brandSizeCombined = brandInputTemporary + "-" + sizeInput.ToString();
+
+                            // Checks if the item entered already exists in the box. Keeps going until item box is full or user enters 'done'
+                            bool itemCheck = false;
+                            if (whileCheck < 12)
+                            {
+                                for (int i = 0; i < brandInput.Length; i++)
+                                {
+                                    if (brandInput[i] == brandSizeCombined)
+                                    {
+                                        Console.WriteLine("Error T-Shirt record already exists");
+                                        itemCheck = false;
+                                        break;
+                                    }
+
+                                    else
+                                    {
+                                        itemCheck = true;
+                                    }
+                                }
+
+                                if (itemCheck == true)
+                                {
+                                    brandInput[whileCheck] = brandSizeCombined;
+                                    Console.WriteLine("Item has stored");
+                                    whileCheck += 1;
+                                }
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Item box is full");
+                            }
+
+                            Console.Write("Do you want to keep going? (Enter 'Done' to stop): ");
+                            end = Console.ReadLine();
+                            if (end == "DONE" || end == "done" || end == "Done")
+                            {
+                                stayA = false;
+                            }
+                        }
+                    }
+
+                    // Option 'B': edit the data
+                    else if (choice == "B" || choice == "b")
+                    {
+                        while (stayB)
+                        {
+                            // Shows item entered in option 'A'
+                            Console.WriteLine("\n*********************** Items ***********************");
+                            for (int i = 0; i < brandInput.Length; i++)
+                            {
+                                Console.WriteLine(brandInput[i]);
+                            }
+                            Console.WriteLine("*****************************************************");
+
+                            // Asks what item to change
+                            Console.Write("Enter the \"brand-size\" you wish to edit: ");
+                            brandInputTemporary = Console.ReadLine();
+
+                            // Checks the index of the entered item
+                            int indexCheckB = Array.IndexOf(brandInput, brandInputTemporary);
+
+                            // Throws an error if the item doesn't exist
+                            if (indexCheckB == -1)
+                            {
+                                throw new ArgumentException("Brand record not found for that entry.");
+                            }
+
+                            // Asks new information
+                            else
+                            {
+                                Console.WriteLine("\nbrand information found\nPlease enter updated information");
+                                Console.Write("Enter brand name ({0} {1} {2}): ", brandjin[0], brandjin[1], brandjin[2]);
+                                brandInputTemporary = Console.ReadLine();
+                                if (brandInputTemporary != brandjin[0] && brandInputTemporary != brandjin[1] && brandInputTemporary != brandjin[2])
+                                {
+                                    throw new ArgumentException("We only have three brands. Enter again.");
+                                }
+
+                                Console.Write("Enter size (1 ~ 4): ");
+                                sizeInput = int.Parse(Console.ReadLine());
+                                if (sizeInput < 1 || sizeInput > 4)
+                                {
+                                    throw new ArgumentException("Size out of range! Enter again.");
+                                }
+                                brandSizeCombined = brandInputTemporary + "-" + sizeInput.ToString();
+
+                                brandInput[indexCheckB] = brandSizeCombined;
+                                Console.WriteLine("The item updated");
+                                Console.WriteLine(brandInput[indexCheckB]);
+                            }
+
+                            Console.Write("Do you want to go back to the main menu (Press 'Enter'): ");
+                            string check = Console.ReadLine();
+                            if (check == "")
+                            {
+                                stayB = false;
+                            }
+                        }
+                    }
+
+                    // Option 'C': shows the item list
+                    else if (choice == "C" || choice == "c")
+                    {
+                        while (stayC)
+                        {
+                            Console.WriteLine("\n*********************** Items ***********************");
+                            for (int i = 0; i < brandInput.Length; i++)
+                            {
+                                Console.WriteLine(brandInput[i]);
+                            }
+                            Console.WriteLine("*****************************************************");
+
+                            Console.Write("Do you want to go back to the main menu (Press 'Enter'): ");
+                            string check = Console.ReadLine();
+                            if (check == "")
+                            {
+                                stayC = false;
+                            }
+                        }
+                    }
+
+                    // Option 'D': allows the user to delete items (chosen item -> None)
+                    else if (choice == "D" || choice == "d")
+                    {
+                        while (stayD)
+                        {
+                            Console.WriteLine("\n*********************** Items ***********************");
+                            for (int i = 0; i < brandInput.Length; i++)
+                            {
+                                Console.WriteLine(brandInput[i]);
+                            }
+                            Console.WriteLine("*****************************************************");
+
+                            Console.Write("Enter the \"brand-size\" you wish to delete: ");
+                            brandInputTemporary = Console.ReadLine();
+
+                            int indexCheckD = Array.IndexOf(brandInput, brandInputTemporary);
+
+                            if (indexCheckD == -1)
+                            {
+                                throw new ArgumentException("Brand record not found for that entry.");
+                            }
+
+                            else
+                            {
+                                Console.Write("brand information found\n!!Are you sure you want to delete Y/N: ");
+                                string deleteConfirm = Console.ReadLine();
+
+                                if (deleteConfirm == "y" || deleteConfirm == "Y")
+                                {
+                                    brandInput[indexCheckD] = "NONE";
+                                    foreach (var i in brandInput)
+                                    {
+                                        Console.WriteLine(i);
+                                        whileCheck = indexCheckD;
+                                    }
+                                }
+                            }
+                            stayD = false;
+                        }
+                    }
+
+                    // Option 'E': ends the application
+                    else if (choice == "E" || choice == "e")
+                    {
+                        goOn = false;
+                        Console.WriteLine("\nHave a great day");
+                    }
+
+                    else
+                    {
+                        throw new ArgumentException("Out of option range (A ~ E)");
+                    }
+                }
+
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }                
+            }            
+        }
+    }
+}
