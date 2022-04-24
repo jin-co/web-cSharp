fd255f1bd5d48eee806e2838881f34b262b05911
jin-co <lilijk@hanmail.net>
2022-04-24 09:26:43 -0400

    Update

---
+++ forms/Assignment4_KwangjinBaek/Assignment_4_KwangjinBaek_1/KBStockMaintenance.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace Assignment_4_KwangjinBaek_1
+{
+    partial class KBStockMaintenance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstStocks = new System.Windows.Forms.ListBox();
+            this.txtStockId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.txtMessages = new System.Windows.Forms.RichTextBox();
+            this.btnClearInputs = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chkIsProcedure = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lstStocks
+            // 
+            this.lstStocks.FormattingEnabled = true;
+            this.lstStocks.ItemHeight = 16;
+            this.lstStocks.Location = new System.Drawing.Point(16, 56);
+            this.lstStocks.Name = "lstStocks";
+            this.lstStocks.Size = new System.Drawing.Size(273, 292);
+            this.lstStocks.TabIndex = 0;
+            this.lstStocks.Click += new System.EventHandler(this.lstStocks_Click);
+            this.lstStocks.SelectedIndexChanged += new System.EventHandler(this.lstStocks_SelectedIndexChanged);
+            // 
+            // txtStockId
+            // 
+            this.txtStockId.Location = new System.Drawing.Point(402, 58);
+            this.txtStockId.Name = "txtStockId";
+            this.txtStockId.ReadOnly = true;
+            this.txtStockId.Size = new System.Drawing.Size(306, 23);
+            this.txtStockId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Stock ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(402, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(306, 23);
+            this.txtName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(402, 269);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(306, 23);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Minutes";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(402, 298);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(306, 23);
+            this.txtMinutes.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Description";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Messages:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(402, 116);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.txtDescription.Size = new System.Drawing.Size(306, 147);
+            this.txtDescription.TabIndex = 3;
+            this.txtDescription.Text = "";
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.ForeColor = System.Drawing.Color.Red;
+            this.txtMessages.Location = new System.Drawing.Point(16, 380);
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.Size = new System.Drawing.Size(843, 103);
+            this.txtMessages.TabIndex = 12;
+            this.txtMessages.Text = "";
+            // 
+            // btnClearInputs
+            // 
+            this.btnClearInputs.Location = new System.Drawing.Point(740, 58);
+            this.btnClearInputs.Name = "btnClearInputs";
+            this.btnClearInputs.Size = new System.Drawing.Size(119, 38);
+            this.btnClearInputs.TabIndex = 7;
+            this.btnClearInputs.Text = "Clear Inputs";
+            this.btnClearInputs.UseVisualStyleBackColor = true;
+            this.btnClearInputs.Click += new System.EventHandler(this.btnClearInputs_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.ForeColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(740, 282);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(119, 38);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(740, 114);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 38);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(740, 170);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(119, 38);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(740, 226);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(119, 38);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(338, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(179, 22);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Stock Maintenance";
+            // 
+            // chkIsProcedure
+            // 
+            this.chkIsProcedure.AutoSize = true;
+            this.chkIsProcedure.Location = new System.Drawing.Point(402, 327);
+            this.chkIsProcedure.Name = "chkIsProcedure";
+            this.chkIsProcedure.Size = new System.Drawing.Size(115, 21);
+            this.chkIsProcedure.TabIndex = 6;
+            this.chkIsProcedure.Text = "Is Procedure?";
+            this.chkIsProcedure.UseVisualStyleBackColor = true;
+            // 
+            // KBStockMaintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 498);
+            this.Controls.Add(this.chkIsProcedure);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClearInputs);
+            this.Controls.Add(this.txtMessages);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStockId);
+            this.Controls.Add(this.lstStocks);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "KBStockMaintenance";
+            this.Text = "Stock Maintenance";
+            this.Load += new System.EventHandler(this.KBStockMintenance_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstStocks;
+        private System.Windows.Forms.TextBox txtStockId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox txtDescription;
+        private System.Windows.Forms.RichTextBox txtMessages;
+        private System.Windows.Forms.Button btnClearInputs;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox chkIsProcedure;
+    }
+}
+
