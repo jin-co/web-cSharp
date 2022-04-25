2651ce5d656bd14d6aabe64d0a5eaf995c45b9fe
jin-co <lilijk@hanmail.net>
2022-04-25 09:15:30 -0400

    Update

---
+++ forms/d3_File/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtRichMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(28, 169);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(318, 23);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(352, 169);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(25, 140);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Exist";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Create";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(190, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(271, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "ListDirectorys";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(352, 216);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "List Forms";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // txtRichMessage
+            // 
+            this.txtRichMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRichMessage.Location = new System.Drawing.Point(28, 12);
+            this.txtRichMessage.Name = "txtRichMessage";
+            this.txtRichMessage.Size = new System.Drawing.Size(399, 116);
+            this.txtRichMessage.TabIndex = 9;
+            this.txtRichMessage.Text = "";
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(433, 12);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(46, 227);
+            this.btnSplit.TabIndex = 10;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 263);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.txtRichMessage);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox txtRichMessage;
+        private System.Windows.Forms.Button btnSplit;
+    }
+}
+
---
+++ forms/d3_File/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(txtInput.Text))
+            {
+                txtRichMessage.Text = "exists";
+            }
+            else
+            {
+                txtRichMessage.Text = "not exitst";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(txtInput.Text))
+                txtRichMessage.Text = "exists";
+            else
+            {
+                Directory.CreateDirectory(txtInput.Text);
+                txtRichMessage.Text = "created";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.Delete(txtInput.Text, true); /*if i put second parameter 
+                                                        * (recursive:)true it will 
+                                                        * delete every sub folder 
+                                                        * with out it if I try to 
+                                                        * delete a folder that has
+                                                        * a sub folder in it, I won't
+                                                        * be able to unless delete 
+                                                        * the sub folder first*/
+                txtRichMessage.Text = "deleted";
+            }
+            catch (Exception ex)
+            {
+                txtRichMessage.Text = "error" + ex.Message; 
+            }
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            txtRichMessage.Text = Path.GetDirectoryName(txtInput.Text) + "\n";
+            txtRichMessage.Text += Path.GetFileName(txtInput.Text) + "\n";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Directory.EnumerateDirectories(txtInput.Text); // this one doesn't have index
+            //string[] file = Directory.EnumerateDirectories(txtInput.Text).ToArray(); // so make it array and join
+            //txtRichMessage.Text = string.Join("\n", file);
+            string[] file1 = Directory.EnumerateDirectories(txtInput.Text, "*", SearchOption.AllDirectories).ToArray(); // to get all sub folder
+            txtRichMessage.Text = string.Join("\n", file1);
+        }
+    }
+}
