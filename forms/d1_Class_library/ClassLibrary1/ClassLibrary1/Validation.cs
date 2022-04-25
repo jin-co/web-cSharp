487c48c92bd371c4ad729502cecc99c795e491e1
jin-co <lilijk@hanmail.net>
2022-04-25 09:14:47 -0400

    Update

---
+++ forms/d1_Class_library/ClassLibrary1/ClassLibrary1/Validation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// collection of validation methods for string, dates  and numerics
+    /// Kwangjin 
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static class Validation
+    {
+        /// <summary>
+        /// verify if a string contains a number ( and can be parsed int one)
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(string value)
+        {
+            double output;
+            return double.TryParse(value, out output);
+        }
+
+        /// <summary>
+        /// return true if the object contains just a number
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(object value)
+        {
+            return IsNumeric(value.ToString());
+        }
+
+        /// <summary>
+        /// returns true if the string contains just an integer
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsInteger(string value)
+        {
+            return IsNumeric(value) && !value.Contains(".");
+
+        }
+    }
+}
