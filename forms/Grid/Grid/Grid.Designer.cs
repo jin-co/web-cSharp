6ca5f5bd7010c395c84705d9dc8cc792fd7340ae
jin-co <lilijk@hanmail.net>
2022-04-26 09:52:52 -0400

    Update

---
+++ forms/Grid/Grid/Grid.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Grid
+{
+    partial class Grid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridView = new System.Windows.Forms.DataGridView();
+            this.txtRecordId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.datDate = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridView
+            // 
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Location = new System.Drawing.Point(0, 0);
+            this.gridView.Name = "gridView";
+            this.gridView.Size = new System.Drawing.Size(565, 150);
+            this.gridView.TabIndex = 0;
+            // 
+            // txtRecordId
+            // 
+            this.txtRecordId.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRecordId.Location = new System.Drawing.Point(12, 227);
+            this.txtRecordId.Name = "txtRecordId";
+            this.txtRecordId.Size = new System.Drawing.Size(174, 24);
+            this.txtRecordId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(12, 257);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(174, 24);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(12, 287);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(174, 24);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // datDate
+            // 
+            this.datDate.CustomFormat = "d MMM yyyy";
+            this.datDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datDate.Location = new System.Drawing.Point(12, 317);
+            this.datDate.Name = "datDate";
+            this.datDate.Size = new System.Drawing.Size(200, 24);
+            this.datDate.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(12, 375);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(165, 375);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 6;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // Grid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.datDate);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtRecordId);
+            this.Controls.Add(this.gridView);
+            this.Name = "Grid";
+            this.Text = "Grid";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridView;
+        private System.Windows.Forms.TextBox txtRecordId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DateTimePicker datDate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
---
+++ forms/Grid/Grid/Grid.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grid
+{
+    public partial class Grid : Form
+    {
+        public Grid()
+        {
+            InitializeComponent();
+        }
+        string fileName = "what.txt";
+        StreamReader reader;
+        StreamWriter writer;
+        string record;
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            record = $"{txtRecordId.TabIndex}\t{txtName.Text}\t" +
+                $"{txtPrice.Text}\t{datDate.Value.ToString()}";
+            using (writer = new StreamWriter(fileName, append: true))
+            {
+                writer.WriteLine(record);
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            gridView.Columns.Clear();
+            gridView.ColumnCount = 4;
+
+            gridView.Columns[0].Name = "ID";
+            gridView.Columns[1].Name = "Name";
+            gridView.Columns[2].Name = "Price";
+            gridView.Columns[3].Name = "DateAdded";
+            using(reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    record = reader.ReadLine();
+                    string[] fields = record.Split('\t');
+                    gridView.Rows.Add(fields);
+                }
+            }
+        }
+    }
+}
