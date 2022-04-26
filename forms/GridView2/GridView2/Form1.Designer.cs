57376d3bdc19b18a44e00d7d17f44c070640efc3
jin-co <lilijk@hanmail.net>
2022-04-26 09:53:19 -0400

    Update

---
+++ forms/GridView2/GridView2/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace GridView2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRecordId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDateReceived = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridView
+            // 
+            this.gridView.AllowUserToAddRows = false;
+            this.gridView.AllowUserToDeleteRows = false;
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Location = new System.Drawing.Point(31, 29);
+            this.gridView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gridView.Name = "gridView";
+            this.gridView.ReadOnly = true;
+            this.gridView.RowHeadersWidth = 62;
+            this.gridView.RowTemplate.Height = 28;
+            this.gridView.Size = new System.Drawing.Size(1191, 321);
+            this.gridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 385);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Record ID";
+            // 
+            // txtRecordId
+            // 
+            this.txtRecordId.Location = new System.Drawing.Point(233, 381);
+            this.txtRecordId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRecordId.Name = "txtRecordId";
+            this.txtRecordId.Size = new System.Drawing.Size(354, 35);
+            this.txtRecordId.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(233, 428);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(354, 35);
+            this.txtName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 431);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "name";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(233, 474);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(354, 35);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 479);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Price";
+            // 
+            // txtDateReceived
+            // 
+            this.txtDateReceived.Location = new System.Drawing.Point(233, 520);
+            this.txtDateReceived.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDateReceived.Name = "txtDateReceived";
+            this.txtDateReceived.Size = new System.Drawing.Size(354, 35);
+            this.txtDateReceived.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 525);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Date Rec\'d";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(671, 360);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(332, 78);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(671, 448);
+            this.btnDisplay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(332, 78);
+            this.btnDisplay.TabIndex = 10;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(1052, 360);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(37, 29);
+            this.lblMessage.TabIndex = 11;
+            this.lblMessage.Text = ";;;;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 823);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtDateReceived);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRecordId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridView);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRecordId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDateReceived;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ forms/GridView2/GridView2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridView2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filename = "stock.txt", record;
+        StreamWriter writer;
+        StreamReader reader;
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            gridView.Rows.Clear();
+
+            gridView.ColumnCount = 4;
+            gridView.Columns[0].Name = "ID";
+            gridView.Columns[1].Name = "Name";
+            gridView.Columns[2].Name = "Price";
+            gridView.Columns[3].Name = "Received";
+
+            using(reader = new StreamReader(filename))
+            {
+                while(!reader.EndOfStream)
+                {
+                    record = reader.ReadLine();
+                    string[] fields = record.Split('\t');
+                    gridView.Rows.Add(fields);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DateTime recieved = Convert.ToDateTime(txtDateReceived.Text);
+            txtDateReceived.Text = recieved.ToString("dd MMM yyyy");
+
+            record = $"{txtRecordId.Text}\t{txtName.Text}\t" +
+                $"{txtPrice.Text}\t{txtDateReceived.Text}";
+
+            try
+            {
+                using (writer = new StreamWriter(filename, append: true))
+                {
+                    writer.WriteLine(record);
+                }
+                lblMessage.Text = "record saved";
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "exception writing to file: " + ex.Message;
+            }            
+        }
+    }
+}
