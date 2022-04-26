5bcffc1d07155b78b442e7c8cf68ecefd0c9a0e6
jin-co <lilijk@hanmail.net>
2022-04-26 09:53:48 -0400

    Update

---
+++ forms/ListBox_2/ListBox_2/ProvinceMaintenance.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace ListBox_2
+{
+    partial class ProvinceMaintenance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstProvinces = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProvinceCode = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCountryCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTaxCode = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFirstLetterPostal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chkIncludesFederalTax = new System.Windows.Forms.CheckBox();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstProvinces
+            // 
+            this.lstProvinces.FormattingEnabled = true;
+            this.lstProvinces.ItemHeight = 16;
+            this.lstProvinces.Location = new System.Drawing.Point(16, 48);
+            this.lstProvinces.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstProvinces.Name = "lstProvinces";
+            this.lstProvinces.Size = new System.Drawing.Size(280, 372);
+            this.lstProvinces.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Province";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Province Code";
+            // 
+            // txtProvinceCode
+            // 
+            this.txtProvinceCode.Location = new System.Drawing.Point(491, 48);
+            this.txtProvinceCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProvinceCode.Name = "txtProvinceCode";
+            this.txtProvinceCode.Size = new System.Drawing.Size(132, 23);
+            this.txtProvinceCode.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(491, 80);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 23);
+            this.txtName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
+            // 
+            // txtCountryCode
+            // 
+            this.txtCountryCode.Location = new System.Drawing.Point(491, 112);
+            this.txtCountryCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCountryCode.Name = "txtCountryCode";
+            this.txtCountryCode.Size = new System.Drawing.Size(132, 23);
+            this.txtCountryCode.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 112);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Country Code";
+            // 
+            // txtTaxCode
+            // 
+            this.txtTaxCode.Location = new System.Drawing.Point(491, 144);
+            this.txtTaxCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTaxCode.Name = "txtTaxCode";
+            this.txtTaxCode.Size = new System.Drawing.Size(132, 23);
+            this.txtTaxCode.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(319, 144);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tax Code";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(491, 176);
+            this.txtRate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(132, 23);
+            this.txtRate.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(319, 176);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Tax Rate";
+            // 
+            // txtFirstLetterPostal
+            // 
+            this.txtFirstLetterPostal.Location = new System.Drawing.Point(491, 236);
+            this.txtFirstLetterPostal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFirstLetterPostal.Name = "txtFirstLetterPostal";
+            this.txtFirstLetterPostal.Size = new System.Drawing.Size(132, 23);
+            this.txtFirstLetterPostal.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(319, 236);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "First Letter Postal";
+            // 
+            // chkIncludesFederalTax
+            // 
+            this.chkIncludesFederalTax.AutoSize = true;
+            this.chkIncludesFederalTax.Location = new System.Drawing.Point(491, 208);
+            this.chkIncludesFederalTax.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkIncludesFederalTax.Name = "chkIncludesFederalTax";
+            this.chkIncludesFederalTax.Size = new System.Drawing.Size(149, 21);
+            this.chkIncludesFederalTax.TabIndex = 14;
+            this.chkIncludesFederalTax.Text = "Includes federal tax";
+            this.chkIncludesFederalTax.UseVisualStyleBackColor = true;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(322, 276);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(301, 144);
+            this.txtMessage.TabIndex = 15;
+            this.txtMessage.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(19, 445);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(122, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(141, 445);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(122, 23);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(263, 445);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 445);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(507, 445);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // ProvinceMaintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 494);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.chkIncludesFederalTax);
+            this.Controls.Add(this.txtFirstLetterPostal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTaxCode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCountryCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtProvinceCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstProvinces);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ProvinceMaintenance";
+            this.Text = "Province Maintenance";
+            this.Load += new System.EventHandler(this.ProvinceMaintenance_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProvinces;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProvinceCode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCountryCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTaxCode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFirstLetterPostal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox chkIncludesFederalTax;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ forms/ListBox_2/ListBox_2/ProvinceMaintenance.cs
@@ -0,0 +1,36 @@
+﻿using Encapsulation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox_2
+{
+    public partial class ProvinceMaintenance : Form
+    {
+        public ProvinceMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        private void ProvinceMaintenance_Load(object sender, EventArgs e)
+        {
+            RebuildListBox();
+        }
+
+        // method to reload the listbox after insert, update and delete
+        private void RebuildListBox()
+        {
+            List<Province> provinces = Province.GetProvinces();
+            provinces = provinces.OrderBy(a => a.Name).ToList();
+
+            lstProvinces.ValueMember = ;
+
+        }
+    }
+}
