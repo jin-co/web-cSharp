8b9e0fb31db7b43304e6318c93427cc215e26214
jin-co <lilijk@hanmail.net>
2022-04-27 09:11:48 -0400

    Update

---
+++ console/WindowsFormsApp3/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Class1
+    {
+        public int MyProperty { get; set; }
+        private int v;
+
+        Class1() 
+        {
+            
+        }
+
+        public static bool IsNumeric(string a)
+        {
+            Regex pattern = new Regex(@"^-?\d+\.\d*?$");
+            return pattern.IsMatch(a);
+        }
+    }
+}
