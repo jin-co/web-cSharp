7b8729defc1591e2e5942cbe22bd7ae86a3e05e3
jin-co <lilijk@hanmail.net>
2022-05-01 09:37:10 -0400

    Update

---
+++ .NET.Core/A2_TransactionRecord/A2_TransactionRecord/Models/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2_TransactionRecord.Models
+{
+    // Company class that stores the information
+    // about company record
+    public class Company
+    {
+        #region Properties
+        [Key]
+        public int CompanyId { get; set; }
+        
+        [Required(ErrorMessage ="Please enter a company name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter an address")]
+        public string Address { get; set; }
+
+        [DisplayName("Ticker Symbol")]
+        [Required(ErrorMessage = "Please enter a ticker symbol")]
+        public string TickerSymbol { get; set; }
+        #endregion
+    }
+}
