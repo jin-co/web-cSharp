a088234d6c6b8af6dca907dbea4584af6ecfebd3
jin-co <lilijk@hanmail.net>
2022-05-02 09:25:03 -0400

    Update

---
+++ .NET.Core/assignment2/New wave/A2_TransactionRecord(beforeMigration)/A2_TransactionRecord/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using A2_TransactionRecord.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2_TransactionRecord.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region  Constructor
+        public HomeController(TransactionContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+
+        #region Properties
+        private TransactionContext context { get; set; }
+        #endregion
+        public IActionResult Index()
+        {
+            var transactions = context.TransactionRecordKbaek7943s
+                .Include(t => t.TrasactionType)
+                .OrderBy(t => t.CompanyName)
+                .ToList();
+            return View(transactions);
+        }
+    }
+}
