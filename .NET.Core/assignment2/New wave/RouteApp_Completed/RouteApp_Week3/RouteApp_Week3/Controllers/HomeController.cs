0165bb69f8a499b9797c91f764b0c4f0bdd14360
jin-co <lilijk@hanmail.net>
2022-05-02 09:25:51 -0400

    Update

---
+++ .NET.Core/assignment2/New wave/RouteApp_Completed/RouteApp_Week3/RouteApp_Week3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouteApp_Week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("Home controller, Index action.");
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return Content("Home controller, About action.");
+        }
+    }
+}
---
+++ .NET.Core/assignment2/New wave/RouteApp_Completed/RouteApp_Week3/RouteApp_Week3/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouteApp_Week3.Controllers
+{
+    [Route("Retail/[controller]/[action]/{id?}")]
+    public class ProductController : Controller
+    {
+        //public IActionResult List(string id = "All")
+        //{
+        //    return Content("Product controller, List action, id: " + id);
+        //}
+
+        //public IActionResult List(string cat, int num)
+        //{
+        //    return Content("Product controller, List action, category: " + cat +
+        //        ", Page number: " + num);
+        //}
+
+        public IActionResult List(string id = "All", int page = 1, string sortBy = "Price")
+        {
+            return Content("Product controller, List action, id: " + id +
+                ", Page number: " + page + ", Sort By: " + sortBy);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return Content("Product controller, Detail action, id: " + id);
+        }
+    }
+}
