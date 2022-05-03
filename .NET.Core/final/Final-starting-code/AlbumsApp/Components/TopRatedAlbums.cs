a12cca0d5f4d7e341613d912788db825c1de1dca
jin-co <lilijk@hanmail.net>
2022-05-03 09:09:29 -0400

    Update

---
+++ .NET.Core/final/Final-starting-code/AlbumsApp/Components/TopRatedAlbums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using AlbumsApp.Models;
+
+namespace AlbumsApp.Components
+{
+    public class TopRatedAlbums : ViewComponent
+    {
+        public TopRatedAlbums(AlbumsDbContext albumDbContext)
+        {
+            _albumDbContext = albumDbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var albums = _albumDbContext.Albums.Where(a => a.Rating >= 8.0).OrderByDescending(a => a.Rating).ToList();
+
+            var viewModel = new TopRatedAlbumsViewModel() { 
+                Albums = albums, 
+                NumberOfAlbumsToDisplay = Math.Min(albums.Count, 5)
+            };
+
+            return View(viewModel);
+        }
+
+        private AlbumsDbContext _albumDbContext;
+    }
+}
---
+++ .NET.Core/final/Final-starting-code/AlbumsApp/Models/Studio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumsApp.Models
+{
+    public class Studio
+    {
+        public int StudioId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+    }
+}
