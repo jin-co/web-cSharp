fa4a4492a74b6efc602e5b1bf3d4cfaf3ad3f6f1
jin-co <lilijk@hanmail.net>
2022-05-03 09:12:14 -0400

    Update

---
+++ .NET.Core/BookstoreApp - Completed/BookstoreApp - V2/BookstoreApp/Models/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookstoreApp.Models
+{
+    public class BookViewModel
+    {
+        public Book Book { get; set; }
+        public string ActiveGenre { get; set; } = "all";
+        public string ActiveAuthor { get; set; } = "all";
+        public string ActivePublisher { get; set; } = "all";
+    }
+}
---
+++ .NET.Core/BookstoreApp - Completed/BookstoreApp - V2/BookstoreApp/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreApp.Models
+{
+    public class Genre
+    {
+        [Key]
+        public string GenreID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
