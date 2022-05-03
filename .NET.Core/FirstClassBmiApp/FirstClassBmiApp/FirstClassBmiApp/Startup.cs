2455e0c7f351fb17bd2a6c4dd4dfd92f16b811a9
jin-co <lilijk@hanmail.net>
2022-05-03 09:13:44 -0400

    Update

---
+++ .NET.Core/FirstClassBmiApp/FirstClassBmiApp/FirstClassBmiApp/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+namespace FirstClassBmiApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // This line of code actually configures the application to handle the MVC approach. It looks towards
+            // finding the "Controllers", "Models", and "Views" folder.
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // This "Default" endpoint is a way of letting the application know where to route the application
+                // to upon launching (i.e. "http://localhost:34532/Bmi/Index/"). The pattern is the important key
+                // part in this configuration. It first calls the BmiController and then calls the "Index" method found
+                // within the controller and returns the Index view.
+                endpoints.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{controller=Bmi}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
