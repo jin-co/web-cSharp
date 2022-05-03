d068b3a2f99ec5f603d5b3336a151f24ea1d7cec
jin-co <lilijk@hanmail.net>
2022-05-03 09:11:05 -0400

    Update

---
+++ .NET.Core/BmiWithBootstrapApp/BmiWithBootstrapApp/BmiWithBootstrapApp/Program.cs
@@ -0,0 +1,21 @@
+using BmiWithBootstrapApp;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace BmiWithBootstrapApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
